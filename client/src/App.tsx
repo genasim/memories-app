75df9cea834b4751ce641f9ad5aff7caffdb2272
Genadi Kolev <genadimkolev@gmail.com>
2023-10-19 23:31:08 +0300

    chore(client): add material-ui; create components
    Create Posts, Post and Form components code
    Setup landing page components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
-import './App.css'
+import { Container, Typography, AppBar, Grow, Grid } from '@material-ui/core'
+import memoriesImage from './assets/images/memories.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Container maxWidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>Memories</Typography>
+        <img src={memoriesImage} alt='memories' height={100} />
+        <Grow in>
+          <Container>
+            <Grid container justifyContent='space-between' alignItems='stretch' spacing={10}>
+              <Grid item xs={12} sm={7}> <Posts /> </Grid>
+              <Grid item xs={12} sm={4}> <Form /> </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </AppBar>
+    </Container>
   )
 }
 
